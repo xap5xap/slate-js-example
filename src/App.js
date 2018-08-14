4dff11379594910c5b1413a35fcf8b64b485617a
Xavier Perez <xap5xap@users.noreply.github.com>
2018-08-14 18:22:05 -0500

    and

--- src/App.js
+++ src/App.js
@@ -1,9 +1,66 @@
 import React, { Component } from 'react';
+import { Editor } from 'slate-react';
+import { Value } from 'slate';
 import logo from './logo.svg';
 import './App.css';
 
+const initialValue = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        object: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            object: 'text',
+            leaves: [
+              {
+                text: 'A line of text in a paragraph'
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+});
+
+function CodeNode(props) {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  );
+}
+
 class App extends Component {
+  state = {
+    value: initialValue
+  };
+
+  onChange = ({ value }) => {
+    console.log('onChange', value);
+    this.setState({ value });
+  };
+
+  onKeyDown = (event, change) => {
+    console.log('onKeyDown', event.key);
+    if (event.key !== '&') return true;
+
+    event.preventDefault();
+    change.insertText('and');
+    return true;
+  };
+
+  renderNode = props => {
+    switch (props.node.type) {
+      case 'code':
+        return <CodeNode {...props} />;
+    }
+  };
+  
   render() {
+    console.log('render', this.state);
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +70,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Editor
+          value={this.state.value}
+          onChange={this.onChange}
+          onKeyDown={this.onKeyDown}
+          renderNode={this.renderNode}
+        />
       </div>
     );
   }
